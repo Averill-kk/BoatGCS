472c116af33bbd49fc7627ef368700763e3d83b7
Maxim <max@14.ru>
2018-10-29 14:33:22 +1000

    add connection window

--- BoatGCS/Entities/GpsDataContext.cs
+++ BoatGCS/Entities/GpsDataContext.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BoatGCS.Utils;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Core.EntityClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +11,19 @@ namespace BoatGCS.Entities
 {
     class GpsDataContext : DbContext
     {
-        public GpsDataContext(): base("DefaultConnection")
+        public GpsDataContext(): base(CreateConnectionString())
         {
 
         }
         public DbSet<GpsData> GpsDatas { get; set; }
+
+        static public string CreateConnectionString()
+        {
+            var entityConnectionStringBuilder = new EntityConnectionStringBuilder();
+            entityConnectionStringBuilder.Provider = "System.Data.SqlClient";
+            entityConnectionStringBuilder.ProviderConnectionString = ConnectionString.Current.Connection;
+            entityConnectionStringBuilder.Metadata = "res://*";
+            return entityConnectionStringBuilder.ToString();
+        }
     }
 }
